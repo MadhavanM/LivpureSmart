b59dd59347651fd723e376208be47ffcfdf3813a
Madhavan <madhavan@planetre.us>
2020-08-05 09:44:27 +0530

    first commit

---
+++ Livpure/Controller/SilverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SilverViewController.swift
+//  Livpure
+//
+//  Created by Madhavan on 04/08/20.
+//  Copyright © 2020 myApp. All rights reserved.
+//
+
+import UIKit
+
+class SilverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
